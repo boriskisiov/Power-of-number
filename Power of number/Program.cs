18d4cabd8648f78f725ecbac84184e2c88736331
boriskisiov <boriskisiov@gmail.com>
2023-10-23 16:42:17 +0300

    Add project files.

---
+++ Power of number/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Power_of_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+            int result = 1;
+            for (int time = 1;  time <= power; time++) {
+            result = result * number;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
